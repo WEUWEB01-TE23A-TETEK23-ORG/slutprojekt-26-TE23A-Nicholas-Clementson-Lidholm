23cc129bb26294262935d0676697a5d7eeb34baa
Nicholas Clementson-Lidholm <you@example.com>
2026-05-07 10:03:18 +0200

    Fixade audio previews till låtar med button

---
+++ script2.js
@@ -0,0 +1,15 @@
+function solution() {
+    document.querySelector("audio").play()
+}
+function sabaton() {
+    document.querySelector("audio").play()
+}
+function dt() {
+    document.querySelector("audio").play()
+}
+function prettymaids() {
+    document.querySelector("audio").play()
+}
+function a7x() {
+    document.querySelector("audio").play()
+}
